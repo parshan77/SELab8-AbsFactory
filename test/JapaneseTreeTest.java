aa6518c6bf7c438bada95b5b71a171c763e247a3
parshan77 <parshanteimouri@yahoo.com>
2022-06-05 22:42:12 +0430

    Tests for: Japanese Tree and khatmi

--- test/JapaneseTreeTest.java
+++ test/JapaneseTreeTest.java
@@ -3,5 +3,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JapaneseTreeTest {
+    @Test
+    void testType() {
+        JapaneseGardenCreator japaneseGardenCreator = new FactoryCreator().createJapaneseGardenCreator();
+        ;
+        assertTrue(japaneseGardenCreator.createTree() instanceof JapaneseTree);
+    }
+
+    @Test
+    void testAge() {
+        JapaneseGardenCreator japaneseGardenCreator = new FactoryCreator().createJapaneseGardenCreator();
+
+        assertTrue(japaneseGardenCreator.createTree().maxAge == 200);
+    }
 
 }
--- test/KhatmiTest.java
+++ test/KhatmiTest.java
@@ -4,4 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class KhatmiTest {
 
+    @Test
+    void testType() {
+        IranianGardenCreator iranianGardenCreator = new FactoryCreator().createIranianGardenCreator();
+        ;
+        assertTrue(iranianGardenCreator.createFlower() instanceof Khatmi);
+    }
+
+    @Test
+    void testLocation() {
+        IranianGardenCreator iranianGardenCreator = new FactoryCreator().createIranianGardenCreator();
+        ;
+        assertTrue(iranianGardenCreator.createFlower().location == "Iran");
+    }
+
 }
