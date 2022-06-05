25f026d8755eaf13d21e266223026340b5fb9773
mahsaShv <Msheikhi@ce.sharif.edu>
2022-06-06 00:00:19 +0430

    -added some classes

--- src/Chenar.java
+++ src/Chenar.java
@@ -1,3 +1,10 @@
 public class Chenar extends AbstractTree{
+    public Chenar(){
+        System.out.println("Chenar tree created.");
+    }
 
+    public Chenar(int maxAge){
+        super.maxAge = maxAge;
+        System.out.println("Chenar tree created.");
+    }
 }
--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,9 @@
 public class Client {
     public static void main(String[] args) {
+        JapaneseGardenCreator japaneseGardenCreator =  new FactoryCreator().createJapaneseGardenCreator();
+        JapaneseTree japaneseTree =  japaneseGardenCreator.createTree();
+
+        System.out.println(japaneseTree.maxAge);
 
 
     }
